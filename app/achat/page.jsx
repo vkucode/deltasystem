11006cf002e21f0ad4bcb20e6c5123d62b29fe43
VKU CODE <ungurean729@gmail.com>
2024-05-12 14:50:14 +0200

    update back navbar and footer

--- app/achat/page.jsx
+++ app/achat/page.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import styles from './achat.module.scss'
 import { getPosts } from "@/_actions/postActions";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 export default async function AchatPage(){
   const { data, errMsg } = await getPosts();
@@ -12,6 +14,7 @@ export default async function AchatPage(){
 
     return (
       <>
+      <Navbar />
         <section className={styles.achatPage}>
         {data.map((item) => (
                 <div>
@@ -27,6 +30,7 @@ export default async function AchatPage(){
                 </div>
             ))}
         </section>
+        <Footer />
       </>
     );
   }
