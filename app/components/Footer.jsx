adf5684f3e9ded1126cd3db423d2dbd63275f604
VKU CODE <ungurean729@gmail.com>
2024-05-19 23:56:19 +0200

    Update navbar and footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Image from 'next/image'
 import styles from './footer.module.scss'
+import { FaMapMarkerAlt, FaPhoneAlt  } from "react-icons/fa";
 
 const Footer = () => {
 
@@ -22,11 +23,11 @@ const Footer = () => {
       <div className={styles.footerInfo}>
         <div className='flex flex-col md:flex-row max-w-7xl md:gap-6 gap-10'>
           <div className='w-full md:w-1/3'>
-            <Image src="/assets/img/footer/logo.png" className='mb-7' width={250} height={200} alt='Delta' />
+            <Image src="/assets/img/accueil/logoOrClear.png" className='mb-7' width={250} height={200} alt='Delta' />
             {/* <div className={styles.separateurInfo}></div> */}
             <ul>
-              <li><a href="https://maps.app.goo.gl/HziQ2BNkxxujq3Az7">Adresse: UAE - DUBAI - Business Bay - Bay Square Marasi Drive the Binary Tower / Étage 20</a></li>
-              <li className='mt-5'><a href="tel:+971585864096">Telephone: +971 58 586 4096</a></li>
+              <li><a href="https://maps.app.goo.gl/HziQ2BNkxxujq3Az7">UAE - DUBAI - Business Bay - Bay Square Marasi Drive the Binary Tower / Étage 20</a></li>
+              <li className='mt-5'><a href="tel:+971585864096">+971 58 586 4096</a></li>
             </ul>
             
           </div>
